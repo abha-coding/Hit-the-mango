ef6ade39d5a8efc9eb1a9dae200f101002f865a3
abha-coding <75477048+abha-coding@users.noreply.github.com>
2021-02-27 23:37:01 +0530

    Add files via upload

---
+++ stone.js
@@ -0,0 +1,31 @@
+class Stone{
+
+    constructor(x,y)
+    {
+        var options = {
+            'isStatic' :false,
+            'restitution':0,
+            'friction':1,
+            'density':1.2
+        }
+        this.width=50;
+        this.height=50;
+        this.body=Bodies.rectangle(x,y,70,50,options);
+
+        this.image = loadImage("stone.png");
+
+        World.add(world,this.body);
+    
+    }
+    display()
+    {
+        var pos=this.body.position;
+        //pos.x=mouseX;
+        //pos.y=mouseY;
+        fill("red");
+        imageMode(CENTER);
+        image(this.image, pos.x, pos.y,this.width,this.height);
+        
+
+    }
+}
